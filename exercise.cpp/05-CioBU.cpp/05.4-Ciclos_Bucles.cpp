1ef9470b5aaa19f23ab799a28ffa38ec8a6a86d5
AndyDav6 <138633572+AndyDav6@users.noreply.github.com>
2025-07-10 12:05:53 -0500

    News file

---
+++ exercise.cpp/05-CioBU.cpp/05.4-Ciclos_Bucles.cpp
@@ -0,0 +1,22 @@
+/* Escriba un programa que tome cada 4 horas
+la temperatura del exterior, leyendola durante
+un periodo de 24 horas. Es decir, debe leer
+6 temperaturas. Calcule la temperatura media
+del dia, la temperatura más alta y la mas baja
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float temperatura, T_media = 0;
+    float T_alta, T_baja = 999;
+
+    for (float i = 1; i <= 24; i++)
+    {
+    }
+
+    return 0;
+}
